d4c0b88ca2eb7a365ccce0b84e87925e4efc0168
Louis Brunner <louis.brunner.fr@gmail.com>
2020-02-29 13:23:28 +0000

    Setup repo

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,13 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  process.env['TOKEN'] = 'ABC'
+  const ip = path.join(__dirname, '..', 'dist', 'index.js')
   const options: cp.ExecSyncOptions = {
-    env: process.env
+    env: process.env,
   }
   console.log(cp.execSync(`node ${ip}`, options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const token: string = core.getInput('token')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(`Setting up OctoKit`)
+    const octokit = new github.GitHub(token)
 
-    core.setOutput('time', new Date().toTimeString())
+    // TODO: finish
   } catch (error) {
     core.setFailed(error.message)
   }
