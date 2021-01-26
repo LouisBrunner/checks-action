607c9898840f4e677622c30d6c7f1d52b9bd838a
Jussi Timperi <236182+Ban3@users.noreply.github.com>
2021-01-26 04:47:55 +0200

    Add annotations to output (#16)

--- src/checks.ts
+++ src/checks.ts
@@ -15,6 +15,7 @@ const unpackInputs = (title: string, inputs: Inputs.Args): Record<string, unknow
       summary: inputs.output.summary,
       text: inputs.output.text_description,
       actions: inputs.actions,
+      annotations: inputs.annotations,
       images: inputs.images,
     };
   }
