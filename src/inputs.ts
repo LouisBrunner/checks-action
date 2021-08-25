dbaab94d03c7c4e920a9ff8a4862d74d9ef3f5e6
Thomas Atchley <thomas.atchley@drizly.com>
2021-08-25 12:08:45 -0400

    truncate by string length, add JSdoc, use camelCase

--- src/inputs.ts
+++ src/inputs.ts
@@ -4,30 +4,6 @@ import * as fs from 'fs';
 
 type GetInput = (name: string, options?: InputOptions | undefined) => string;
 
-// getBinarySize and truncateToBinarySize were adapted from unlicensed work here:
-// https://github.com/lemonde/utf8-binary-cutter/blob/master/lib/utf8-binary-cutter.js
-function getBinarySize(s: string): number {
-  // Get binary size of a UTF8 string
-  return Buffer.byteLength(s || '', 'utf8');
-}
-
-function truncateToBinarySize(string: string, binaryMaxSize: number): string {
-  // Truncate a UTF8 string to less than or equal to binaryMaxSize
-  const DEFAULT_TRUNCATE_STRING = '...';
-  const DEFAULT_TRUNCATE_STRING_BINARY_SIZE = Buffer.byteLength(DEFAULT_TRUNCATE_STRING, 'utf8');
-
-  string = string || '';
-  if (getBinarySize(string) <= binaryMaxSize) return string; // OK
-
-  // we'll use buffer.write to truncate,
-  // since it doesn't overflow neither write partial UTF-8 characters.
-  const truncatingBuffer = Buffer.alloc(binaryMaxSize - DEFAULT_TRUNCATE_STRING_BINARY_SIZE);
-  const writtenBinaryLength = truncatingBuffer.write(string, 'utf8');
-  const truncatedString =
-    truncatingBuffer.toString('utf8', 0, writtenBinaryLength) + DEFAULT_TRUNCATE_STRING;
-  return truncatedString;
-}
-
 const parseJSON = <T>(getInput: GetInput, property: string): T | undefined => {
   const value = getInput(property);
   if (!value) {
@@ -41,51 +17,105 @@ const parseJSON = <T>(getInput: GetInput, property: string): T | undefined => {
   }
 };
 
-function reverseString(input_string: string): string {
-  return input_string.split('').reverse().join('');
+/**
+ * Formats user-inputted JSON variables into markdown
+ * @param {Inputs.Output | undefined} outputJson - the JSON object to transform in to markdown
+ * @param {string} [outputStartIndicator] - a pre-defined string to be used in machine-parsing
+ * @return {string} - a markdown JSON string
+ */
+function buildJsonMarkdnown(
+  outputJson: Inputs.Output | undefined,
+  outputStartIndicator = '---- BEGIN CHECK OUTPUT ----',
+): string {
+  const prettyOutputJSON = JSON.stringify(outputJson, null, 2);
+  const jsonOutputMarkdown = `
+# JSON Outputs
+\`\`\`json ${outputStartIndicator}
+${prettyOutputJSON}
+\`\`\``;
+
+  const variablesExist = prettyOutputJSON != '{}';
+  const result = variablesExist ? jsonOutputMarkdown : '';
+  return result;
 }
 
-function buildOutputDescription(
-  output_description_md: string,
-  json_vars: string,
-  max_allowed_bytes: number,
+/**
+ * Truncates a string to at most maxChars
+ * @param {string} inputString - The string to truncate
+ * @param {number} maxChars - How many characters should inputString be truncated to
+ * @param {boolean} [removeFromBeginning=true] - Controls whether excess characters should be removed from the beginning or end of the string
+ * @param {string} [removalIndicator="..."] - A string to indicate where the truncation of the text occured. Will be substituted in place of truncated text
+ * @return {string} a string which is no longer than maxChars in length
+ */
+function truncateStringChars(
+  inputString: string,
+  maxChars: number,
+  removeFromBeginning = true,
+  removalIndicator = '...',
 ): string {
-  // DESCRIPTION:
-  // Builds the output.text parameter of the GitHub Check, prioritizing the JSON variables when the
-  // file size is over GitHub's allowed maximum
-  // INPUTS:
-  // output_description_md: a string of the description file
-  // json_vars: a markdown string of the JSON variables we want to append
-  // max_allowed_bytes: how many bytes should be allowed
-  // RETURNS:
-  // a formatted output description file of no more than max_allowed_bytes
+  const shortEnough = inputString.length <= maxChars;
 
-  const output_file_size = getBinarySize(output_description_md);
-  const json_output_markdown_size = getBinarySize(json_vars);
-
-  if (output_file_size + json_output_markdown_size <= max_allowed_bytes) {
-    // everything is within bounds, carry on
-    return output_description_md + json_vars;
-  } else {
-    // size is too large, truncate the report, preserving the ending (tail)
-    const leftover_space = max_allowed_bytes - json_output_markdown_size;
-
-    const reversed_description = reverseString(output_description_md);
-    const truncated_description = truncateToBinarySize(reversed_description, leftover_space);
-    const corrected_description = reverseString(truncated_description);
-
-    return corrected_description + json_vars;
+  if (shortEnough) {
+    return inputString;
   }
-  // TODO: Handle edge case where JSON is larger than max_limit
+
+  // String is too long, we need to trim it
+  const seperatorLength = removalIndicator.length;
+  const numCharsToKeep = maxChars - seperatorLength;
+
+  if (removeFromBeginning) {
+    const startIndex = inputString.length - numCharsToKeep;
+    const endIndex = inputString.length;
+    const shortendString = removalIndicator + inputString.substring(startIndex, endIndex);
+    return shortendString;
+  } else {
+    const shortendString = inputString.substring(0, numCharsToKeep) + removalIndicator;
+    return shortendString;
+  }
+}
+
+/**
+ * Builds output.text_description and output.summary values
+ * @param {string} outputSummary - The output summary as provided by the user
+ * @param {string} outputTextDescriptionInput - the detailed description as provided by the user
+ * @param {string} outputMarkdownFileLocation - the path to a markdown report to be included in the summary
+ * @param {Inputs.Output | undefined} outputJsonVars - a JSON object to be added to the report
+ * @param {number} [maxChars=65535] - maximum number of characters to be allowed in output.summary and output.text. Defaults to the GitHub's limit
+ * @return {string[]} a string array containing values for output.summary and output.text_description
+ */
+function buildOutputForGitHubCheck(
+  outputSummaryInput = '',
+  outputTextDescriptionInput = '',
+  outputMarkdownFileLocation = '',
+  outputJsonVars: Inputs.Output | undefined,
+  maxChars = 65535,
+): string[] {
+  // Get raw values, preferring to read from a file if one is preovided
+  const outputDescription = outputMarkdownFileLocation
+    ? fs.readFileSync(outputMarkdownFileLocation, 'utf8')
+    : outputTextDescriptionInput;
+  const jsonVars = buildJsonMarkdnown(outputJsonVars);
+
+  // Truncate report if too long or we need to append JSON variables
+  const shortEnough = outputDescription.length + jsonVars.length <= maxChars;
+  const truncatedReport = shortEnough
+    ? outputDescription
+    : truncateStringChars(outputDescription, maxChars - jsonVars.length, true);
+
+  // Collate results. jsonVars may be empty
+  const combinedOutputDescription = truncatedReport + jsonVars;
+  const summary = truncateStringChars(outputSummaryInput, maxChars, false);
+
+  // [output.summary, output.text_description]
+  const result = [summary, combinedOutputDescription];
+  return result;
 }
 
 export const parseInputs = (getInput: GetInput): Inputs.Args => {
   const repo = getInput('repo');
   const sha = getInput('sha');
   const token = getInput('token', {required: true});
-  const output_text_description_file = getInput('output_text_description_file');
-  const outputs_start_indicator = '---- BEGIN CHECK OUTPUT ----';
-  const GITHUB_CHECK_DESCRIPTION_MAX_SIZE_BYTES = 65536; // 64 KiB
+  const outputTextDescriptionFile = getInput('output_text_description_file');
 
   const name = getInput('name');
   const checkIDStr = getInput('check_id');
@@ -132,7 +162,7 @@ export const parseInputs = (getInput: GetInput): Inputs.Args => {
   }
 
   const output = parseJSON<Inputs.Output>(getInput, 'output');
-  const output_json = parseJSON<Inputs.Output>(getInput, 'output_json');
+  const outputJson = parseJSON<Inputs.Output>(getInput, 'output_json');
   const annotations = parseJSON<Inputs.Annotations>(getInput, 'annotations');
   const images = parseJSON<Inputs.Images>(getInput, 'images');
   const actions = parseJSON<Inputs.Actions>(getInput, 'actions');
@@ -141,37 +171,12 @@ export const parseInputs = (getInput: GetInput): Inputs.Args => {
     throw new Error(`missing value for 'action_url'`);
   }
 
-  const pretty_output_json = JSON.stringify(output_json, null, 2);
-  const json_output_markdown = `
-# JSON Outputs
-\`\`\`json ${outputs_start_indicator}
-${pretty_output_json}
-\`\`\``;
-
-  const should_append_json = pretty_output_json != '{}';
-
-  if (output && output_text_description_file && should_append_json) {
-    // we have a summary file and JSON variables to append
-    const output_file_md = fs.readFileSync(output_text_description_file, 'utf8');
-    output.text_description = buildOutputDescription(
-      output_file_md,
-      json_output_markdown,
-      GITHUB_CHECK_DESCRIPTION_MAX_SIZE_BYTES,
-    );
-  } else if (output && output_text_description_file && !should_append_json) {
-    // we only have the description file
-    const output_file_md = fs.readFileSync(output_text_description_file, 'utf8');
-    output.text_description = buildOutputDescription(
-      output_file_md,
-      '',
-      GITHUB_CHECK_DESCRIPTION_MAX_SIZE_BYTES,
-    );
-  } else if (output && should_append_json && !output_text_description_file) {
-    // we only have JSON
-    output.text_description = buildOutputDescription(
-      '',
-      json_output_markdown,
-      GITHUB_CHECK_DESCRIPTION_MAX_SIZE_BYTES,
+  if (output) {
+    [output.summary, output.text_description] = buildOutputForGitHubCheck(
+      output.summary,
+      output.text_description,
+      outputTextDescriptionFile,
+      outputJson,
     );
   }
 
